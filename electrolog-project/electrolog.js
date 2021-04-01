2f3627742b61b0c7ffbcc11e6b94a3a99e1c6e1a
yardentzaraf <74673812+yardentzaraf@users.noreply.github.com>
2021-04-01 11:28:56 +0300

    Add files via upload

---
+++ electrolog-project/electrolog.js
@@ -0,0 +1,13 @@
+
+//open login popup window:
+var popupMessage1= document.getElementById('login-popup');
+function loginButton() {
+    popupMessage1.style.display='grid';
+}
+
+//open sign-up popup window:
+var popupMessage2= document.getElementById('signUp-popup');
+function signUpButton() {
+    popupMessage2.style.display='grid';
+}
+
